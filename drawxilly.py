54c9c3b31dbb30fe6a1bd42cd8c0a100374ebb22
一个狗 <imjustadog@outlook.com>
2018-05-25 16:57:18 +0800

    accelerate draw visdom

--- drawxilly.py
+++ drawxilly.py
@@ -9,34 +9,32 @@ def receivedata(win,viz):
 	    #fb = open("/dev/xillybus_read_32", "rb")
 	    fb = open("/home/adoge/xillybus_save_32", "rb")
 	    x = 0
+            datax = []
+            datay = []
 	    while True:
-		data = fb.read(262144)
+		data = fb.read(4)
 		if not data:
 		    break
-                datax = []
-                datay = []
-                total = 262144 / 4
-                for i in range(total):
-		    ch1, ch2 = struct.unpack('<HH', data[(i*4):(i*4+4)])
-		    ch1 = (float(ch1) - 8192) / 8192 * 2.5
-		    ch2 = (float(ch2) - 8192) / 8192 * 2.5
-		    ch1 = float(ch1)
-		    ch2 = float(ch2)
-                    x = x + 1
-                    datax.append(x * 0.0000001)
-                    datay.append(ch1)
-		viz.updateTrace(
-	    		X=np.array(datax),
-	    		Y=np.array(datay),
-	    		win=win,
-                        name = "1"
-		)
-		#viz.updateTrace(
-	    	#	X=np.array([x]),
-	    	#	Y=np.array([ch2]),
-	    	#	win=win,
-                #        name = "2"
-		#)
+		ch1, ch2 = struct.unpack('<HH', data)
+		ch1 = (float(ch1) - 8192) / 8192 * 2.5
+		ch2 = (float(ch2) - 8192) / 8192 * 2.5
+		ch1 = float(ch1)
+		ch2 = float(ch2)
+                x = x + 1
+                datax.append(x * 0.0000001)
+                datay.append(ch1)
+	    viz.updateTrace(
+                X=np.array(datax),
+                Y=np.array(datay),
+                win=win,
+                name = "1"
+	    )
+            #viz.updateTrace(
+	    #	X=np.array([x]),
+	    #	Y=np.array([ch2]),
+	    #	win=win,
+            #        name = "2"
+	    #)
 		
 	    fb.close()
 	    #time.sleep(1)
