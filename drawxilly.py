a3e16a4d2b3f997cc168d12acf469e62792efd08
一个狗 <imjustadog@outlook.com>
2018-05-30 23:32:35 +0800

    auto draw

--- drawxilly.py
+++ drawxilly.py
@@ -3,55 +3,82 @@ import visdom
 import numpy as np
 import struct
 import time
+import sys
+import os
 
-def receivedata(win,viz):
-	#while True:
-	    #fb = open("/dev/xillybus_read_32", "rb")
-	    fb = open("/home/adoge/xillybus_save_32", "rb")
-	    x = 0
-            datax = []
-            datay = []
-	    while True:
-		data = fb.read(4)
-		if not data:
-		    break
-		ch1, ch2 = struct.unpack('<HH', data)
-		ch1 = (float(ch1) - 8192) / 8192 * 2.5
-		ch2 = (float(ch2) - 8192) / 8192 * 2.5
-		ch1 = float(ch1)
-		ch2 = float(ch2)
-                x = x + 1
-                datax.append(x * 0.0000001)
-                datay.append(ch1)
-	    viz.updateTrace(
-                X=np.array(datax),
-                Y=np.array(datay),
-                win=win,
-                name = "1"
-	    )
-            #viz.updateTrace(
-	    #	X=np.array([x]),
-	    #	Y=np.array([ch2]),
-	    #	win=win,
-            #        name = "2"
-	    #)
-		
-	    fb.close()
-	    #time.sleep(1)
+class Draw_Visdom:
+    def __init__(self):
+        self.folderlist = os.listdir("/home/adoge/eclipse-workspace/savexilly")
+        tFolder = threading.Thread(name="folderDetector", target=self.detectfolder)
+        tFolder.start()
+
+ 
+    def detectfolder(self):
+        while True:
+            flag = False
+            folderlisttemp = os.listdir("/home/adoge/eclipse-workspace/savexilly")
+            for folder in folderlisttemp:
+                if folder not in self.folderlist:
+                    flag = True
+                    break
+            if flag == True:
+                break
+            time.sleep(1)
+        self.foldername = folder
+        tData = threading.Thread(name="dataGenerator", target=self.detectdata)
+        tData.start()
+    
+    
+
+    def detectdata(self):
+        folderpath = "/home/adoge/eclipse-workspace/savexilly" + os.path.sep + self.foldername + os.path.sep
+        count = 0 
+	while True:
+	    datalisttemp = os.listdir(folderpath)
+            if str(count) in datalisttemp:
+                path = folderpath + str(count)
+                viz = visdom.Visdom()
+                win = viz.line(
+                    X=np.array([0]),
+                    Y=np.array([0]),
+                    name="1"
+                )
+                #viz.line(
+                #    X=np.array([0]),
+                #    Y=np.array([0]),
+                #    win = win,
+                #    update = "new",
+                #    name="2"
+                #)
+                self.drawline(path,viz,win)
+                count = count + 1                 
+	    time.sleep(0.5)
+    
+    def drawline(self,path,viz,win):
+        fb = open(path, "rb")
+	x = 0
+        datax = []
+        datay = []
+	while True:
+            data = fb.read(4)
+	    if not data:
+                break
+	    ch1, ch2 = struct.unpack('<HH', data)
+	    ch1 = (float(ch1) - 8192) / 8192 * 2.5
+	    ch2 = (float(ch2) - 8192) / 8192 * 2.5
+	    ch1 = float(ch1)
+	    ch2 = float(ch2)
+            datay.append(ch1)
+            x = x + 1
+
+	viz.updateTrace(
+            X=np.array(range(x)) * 0.0000001,
+            Y=np.array(datay),
+            win=win,
+            name = "1"
+	)
+	fb.close()
 
 
-viz = visdom.Visdom()
-win=viz.line(
-    X=np.array([0]),
-    Y=np.array([0]),
-    name="1"
-)
-#viz.line(
-#    X=np.array([0]),
-#    Y=np.array([0]),
-#    win = win,
-#    update = "new",
-#    name="2"
-#)
-tData = threading.Thread(name="dataGenerator", target=receivedata, args=(win,viz))
-tData.start()
+if __name__=="__main__":
+    draw = Draw_Visdom()
